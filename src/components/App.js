652be4a750e5bb63926bc134ce41c82d4995e395
Aria Kim <shoo7830@gmail.com>
2019-07-21 23:23:13 +0900

    todo list

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import PageTemplate from './PageTemplate';
+
+class App extends Component {
+    render() {
+        return(
+            <PageTemplate>안녕하세요</PageTemplate>
+        )
+    }
+}
+
+export default App;
