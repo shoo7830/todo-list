38d921ea941098f57a2b92b78dd50c80c2cc18a9
Aria Kim <shoo7830@gmail.com>
2019-07-22 23:12:32 +0900

    todo data add

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,52 @@ import TodoInput from './TodoInput';
 import TodoList from './TodoList';
 
 class App extends Component {
+    state = {
+        input: '',
+        todos: [
+            {id: 0, text: '리액트 공부하기', done: true},
+            {id: 1, text: '컴포넌트 스타일링 해보기', done: false}
+        ]
+    }
+
+    id = 1
+    getId = () => {
+        return ++this.id;
+    }
+
+    handleChange = (e) => {
+        const { value } = e.target;
+        this.setState({
+            input: value
+        });
+    }
+
+    handleInsert = () => {
+        const { todos, input } = this.state;
+
+        const newTodo = {
+            text: input,
+            done: false,
+            id: this.getId()
+        };
+
+        this.setState({
+            todos: [...todos, newTodo],
+            input: ''
+        });
+    }
+
     render() {
+        const { input, todos } = this.state;
+        const {
+            handleChange,
+            handleInsert
+        } = this;
+
         return(
             <PageTemplate>
-                <TodoInput />
-                <TodoList />
+                <TodoInput onChange={handleChange} onInesert={handleInsert} value={input} />
+                <TodoList todos={todos} />
             </PageTemplate>
         )
     }
