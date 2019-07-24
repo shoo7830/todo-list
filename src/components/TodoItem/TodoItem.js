452113ba7d3db279710358acbe1839dd2061cbe8
Aria Kim <shoo7830@gmail.com>
2019-07-24 23:30:50 +0900

    todo data remove

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -11,7 +11,10 @@ class TodoItem extends Component {
             <div className={cx('todo-item')} onClick={onToggle}>
                 <input className={cx('tick')} type="checkbox" checked={done} readOnly/>
                 <div className={cx('text', {done})}>{children}</div>
-                <div className={cx('delete')} onClick={onRemove}>[지우기]</div>
+                <div className={cx('delete')} onClick={(e) => {
+                    onRemove();
+                    e.stopPropagation();
+                }}>[지우기]</div>
             </div>
         )
     }
