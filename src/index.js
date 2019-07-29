22835458d6004a58ec563412c3954679745d3e31
Aria Kim <shoo7830@gmail.com>
2019-07-29 23:42:06 +0900

    todo add redux

--- src/index.js
+++ src/index.js
@@ -4,9 +4,16 @@ import './styles/main.scss';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import modules from './modules';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const store = createStore(modules, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+ReactDOM.render (
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
 serviceWorker.unregister();
